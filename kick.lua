2014162b32072f1ca29e322bcf6d142cea4d0385
medusakands <levaninjago@gmail.com>
2025-01-17 11:14:26 +0700

    Update kick.lua

--- kick.lua
+++ kick.lua
@@ -1,5 +1,22 @@
-local player = game.Players.LocalPlayer
+local _0x1 = game.Players.LocalPlayer
 
-if player then
-    player:Kick("ТЫ ЗАБАНЕН СЫН ШЛЮХИ")
+if _0x1 then
+    local function encrypt_message(msg)
+        local encrypted = ""
+        for i = 1, #msg do
+            encrypted = encrypted .. string.char(string.byte(msg, i) + 5) -- Сдвиг на 5
+        end
+        return encrypted
+    end
+
+    local function decrypt_message(encrypted_msg)
+        local decrypted = ""
+        for i = 1, #encrypted_msg do
+            decrypted = decrypted .. string.char(string.byte(encrypted_msg, i) - 5) -- Обратный сдвиг
+        end
+        return decrypted
+    end
+
+    local _0x2 = encrypt_message("ТЫ ЗАБАНЕН СЫН ШЛЮХИ")
+    _0x1:Kick(decrypt_message(_0x2))
 end
